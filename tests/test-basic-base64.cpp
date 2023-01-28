01f677cc2aa4f34f2573ef08010735f51c238fa2
harit-pc <haritsahm@gmail.com>
2023-01-28 20:41:39 +0700

    Update testing cases

--- tests/test-basic-base64.cpp
+++ tests/test-basic-base64.cpp
@@ -45,4 +45,99 @@ TEST(Base64, basic)
     std::cout << "reference: " << rest2_reference << std::endl;
     std::cout << "decoded:   " << rest2_decoded << std::endl
               << std::endl;
+
+    std::string rest3_original = "abcde1234";
+    std::string rest3_reference = "YWJjZGUxMjM0";
+
+    std::string rest3_encoded = base64_encode(rest3_original);
+    std::string rest3_decoded = base64_decode(rest3_reference);
+
+    ASSERT_EQ(rest3_decoded, rest3_original);
+
+    std::cout << "encoded:   " << rest3_encoded << std::endl;
+    std::cout << "reference: " << rest3_reference << std::endl;
+    std::cout << "decoded:   " << rest3_decoded << std::endl
+              << std::endl;
+
+    std::string rest4_original = "abcde1234+";
+    std::string rest4_reference = "YWJjZGUxMjM0Kw==";
+
+    std::string rest4_encoded = base64_encode(rest4_original);
+    std::string rest4_decoded = base64_decode(rest4_reference);
+
+    ASSERT_EQ(rest4_decoded, rest4_original);
+
+    std::cout << "encoded:   " << rest4_encoded << std::endl;
+    std::cout << "reference: " << rest4_reference << std::endl;
+    std::cout << "decoded:   " << rest4_decoded << std::endl
+              << std::endl;
+
+    std::string rest5_original = "abcde1234/";
+    std::string rest5_reference = "YWJjZGUxMjM0Lw==";
+
+    std::string rest5_encoded = base64_encode(rest5_original);
+    std::string rest5_decoded = base64_decode(rest5_reference);
+
+    ASSERT_EQ(rest5_decoded, rest5_original);
+
+    std::cout << "encoded:   " << rest5_encoded << std::endl;
+    std::cout << "reference: " << rest5_reference << std::endl;
+    std::cout << "decoded:   " << rest5_decoded << std::endl
+              << std::endl;
+
+    std::string rest6_original = "abcde1234+/";
+    std::string rest6_reference = "YWJjZGUxMjM0Ky8=";
+
+    std::string rest6_encoded = base64_encode(rest6_original);
+    std::string rest6_decoded = base64_decode(rest6_reference);
+
+    ASSERT_EQ(rest6_decoded, rest6_original);
+
+    std::cout << "encoded:   " << rest6_encoded << std::endl;
+    std::cout << "reference: " << rest6_reference << std::endl;
+    std::cout << "decoded:   " << rest6_decoded << std::endl
+              << std::endl;
+
+    std::string rest7_original = "123456";
+    std::string rest7_reference = "MTIzNDU2";
+
+    std::string rest7_encoded = base64_encode(rest7_original);
+    std::string rest7_decoded = base64_decode(rest7_reference);
+
+    ASSERT_EQ(rest7_decoded, rest7_original);
+
+    std::cout << "encoded:   " << rest7_encoded << std::endl;
+    std::cout << "reference: " << rest7_reference << std::endl;
+    std::cout << "decoded:   " << rest7_decoded << std::endl
+              << std::endl;
+
+    std::string rest8_original = "";
+    std::string rest8_reference = "";
+
+    std::string rest8_encoded = base64_encode(rest8_original);
+    std::string rest8_decoded = base64_decode(rest8_reference);
+
+    ASSERT_EQ(rest8_decoded, rest8_original);
+
+    std::cout << "encoded:   (empty)" << rest8_encoded << std::endl;
+    std::cout << "reference: (empty)" << rest8_reference << std::endl;
+    std::cout << "decoded:   (empty)" << rest8_decoded << std::endl
+              << std::endl;
+}
+
+TEST(Base64, invalid_encoded)
+{
+    // Test with input outside base64_chars
+
+    std::string rest0_original = "YWJ2Z?GMyMw==";
+
+    try
+    {
+        base64_decode(rest0_original);
+        FAIL() << "Expected std::runtime_error";
+    }
+    catch (std::runtime_error const &err)
+    {
+        EXPECT_EQ(err.what(), std::string("Input is not valid base64-encoded data."));
+    }
 }
