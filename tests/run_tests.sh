2f677d79876ff95124c390db25ffcb33003d7ef1
haritsah <haritsah@qlue.id>
2023-01-28 18:06:41 +0700

    Add codecove reporting

--- tests/run_tests.sh
+++ tests/run_tests.sh
@@ -3,3 +3,4 @@ cd /workspace/image-resizer-app/build/
 cmake -DRUN_TESTS=ON -DCMAKE_BUILD_TYPE=Debug .. && make
 ctest --output-on-failure --progress && lcov --capture --directory . --output-file coverage.info
 lcov --remove coverage.info '/usr/*' --output-file coverage.info
+bash <(curl -s https://codecov.io/bash) -f coverage.info || echo "Codecov did not collect coverage reports"
