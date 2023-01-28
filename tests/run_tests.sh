7fe2ab07ca38e86c053b77fa501f1973b1687d58
haritsah <haritsah@qlue.id>
2023-01-28 18:47:56 +0700

    Move codecov reporting to actions

--- tests/run_tests.sh
+++ tests/run_tests.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 cd /workspace/image-resizer-app/build/
-curl -Os https://uploader.codecov.io/latest/linux/codecov
-chmod +x codecov
 
 cmake -DRUN_TESTS=ON -DCMAKE_BUILD_TYPE=Debug .. && make
 ctest --output-on-failure --progress && lcov --capture --directory . --output-file coverage.info
 lcov --remove coverage.info '/usr/*' --output-file coverage.info
 
-./codecov -f coverage.info || echo "Codecov did not collect coverage reports"
+mkdir -p /mnt/coverage && cp coverage.info /mnt/coverage/coverage.info
+
+
