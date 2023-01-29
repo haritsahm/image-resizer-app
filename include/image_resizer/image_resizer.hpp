bb8f6f650df69603cd617b22d8847987618532ff
haritsah <haritsah@qlue.id>
2023-01-29 15:54:24 +0700

    Update docstring

--- include/image_resizer/image_resizer.hpp
+++ include/image_resizer/image_resizer.hpp
@@ -28,7 +28,15 @@ public:
     Error process(const std::string &encoded_input, std::string &encoded_output);
 
 private:
+    /// @brief Decode image from base64 string data
+    /// @param byte_string encoded string image
+    /// @return Decoded image in cv::Mat format
     cv::Mat decode_image(const std::string &byte_string);
+
+    /// @brief Encode cv::Mat image to base64 string data
+    /// @param image input image to be encoded
+    /// @param type Image encoding/compression type
+    /// @return Encoded image in string format
     std::string encode_image(const cv::Mat &image, const std::string &type);
 };
 
